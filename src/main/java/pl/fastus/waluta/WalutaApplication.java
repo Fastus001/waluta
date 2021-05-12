9da9a905996e328ead5e416f49aa9b5a04e7b217
Fastus <fastus6666@gamil.com>
2021-05-12 13:24:41 +0200

    first commit

---
+++ src/main/java/pl/fastus/waluta/WalutaApplication.java
@@ -0,0 +1,13 @@
+package pl.fastus.waluta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalutaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WalutaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/fastus/waluta/WalutaApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.fastus.waluta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalutaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
